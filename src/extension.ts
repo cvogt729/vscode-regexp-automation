0df1f808d3459919ec73bbd4205579eda3cb722e
Cameron Vogt <cvogt729@gmail.com>
2021-12-07 18:24:19 -0600

    delete commented code

--- src/extension.ts
+++ src/extension.ts
@@ -293,37 +293,3 @@ export function activate(context: vscode.ExtensionContext) {
   //TODO - add command regexp.modifyWorkspace
 }
 export function deactivate(){}
-/*
-function runSingleRule(textEditor: vscode.TextEditor, _edit: vscode.TextEditorEdit, args?: any) {
-  let editP = new ReplaceRulesEditProvider(textEditor);
-  if (args) {
-    let ruleName = args['ruleName'];
-    editP.runSingleRule(ruleName);
-  } else {
-    editP.pickRuleAndRun();
-  }
-  return;
-}
-
-function runRuleset(textEditor: vscode.TextEditor, _edit: vscode.TextEditorEdit, args?: any) {
-  let editP = new ReplaceRulesEditProvider(textEditor);
-  if (args) {
-    let rulesetName = args['rulesetName'];
-    editP.runRuleset(rulesetName);
-  } else {
-    editP.pickRulesetAndRun();
-  }
-  return;
-}
-
-function pasteReplace(textEditor: vscode.TextEditor, _edit: vscode.TextEditorEdit, args?: any) {
-  let editP = new ReplaceRulesEditProvider(textEditor);
-  if (args) {
-    let ruleName = args['ruleName'];
-    editP.pasteReplace(ruleName);
-  } else {
-    editP.pickRuleAndPaste();
-  }
-  return;
-}
-*/
